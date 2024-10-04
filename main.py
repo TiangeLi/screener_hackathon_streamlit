a7564f37fb2128fdb6d6174d62168894c278500d
Tiange Li <5252737+TiangeLi@users.noreply.github.com>
2024-10-04 12:00:37 -0400

    download results

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ if "screening_question" not in st.session_state:
     st.session_state.screening_question = ""
 if "exclusion_criteria" not in st.session_state:
     st.session_state.exclusion_criteria = ""
+if "final_output" not in st.session_state:
+    st.session_state.final_output = []
 
 
 with st.sidebar:
@@ -22,6 +24,16 @@ with st.sidebar:
         st.session_state.articles = articles
         length = len(st.session_state.articles)
         st.write(f"Number of articles to screen: {length}")
+    if st.session_state.final_output:
+        '---'
+        csv = pd.DataFrame(st.session_state.final_output)
+        export = csv.to_csv(index=False).encode('utf-8')
+        st.download_button(
+            label=':red[Download results]',
+            data=export,
+            file_name='results.csv',
+            mime='text/csv'
+        )
 
 col1, col2 = st.columns(2)
 
@@ -33,11 +45,12 @@ with col2:
     st.write("Exclusion Criteria")
     st.session_state.exclusion_criteria = st.text_area("Enter the exclusion criteria")
 
+
 if st.button("Extract Data"):
     info = st.empty()
     inc_cnt, exc_cnt = 0, 0
     with st.spinner("Extracting data..."):
-        batch_size = 10
+        batch_size = 100
         for i in range(0, len(st.session_state.articles), batch_size):
             batch = st.session_state.articles[i:i+batch_size]
             to_extract = [{"article": article, "screening_question": st.session_state.screening_question} for article in batch]
@@ -49,6 +62,8 @@ if st.button("Extract Data"):
             exc_cnt += len([s for s in screened if not s["include"]])
             
             final = [{**article, "include": s["include"], "original_text": original} for article, s, original in zip(extracted, screened, batch)]
+            st.session_state.final_output.extend(final)
             info.markdown(f"### Screened {inc_cnt + exc_cnt} articles. {inc_cnt} included and {exc_cnt} excluded.")
-            for article in final:
-                st.write(article)
+            with st.expander("View last 20 extracted articles"):
+                for article in final[-20:]:
+                    st.write(article)
