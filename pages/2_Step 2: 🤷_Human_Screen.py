2eab183b9d49ce6ca65e7480ee604bfde47eceed
Tiange Li <5252737+TiangeLi@users.noreply.github.com>
2024-10-04 16:38:08 -0400

    reqs for deployment

--- pages/2_Step 2: 🤷_Human_Screen.py
+++ pages/2_Step 2: 🤷_Human_Screen.py
@@ -1,9 +1,13 @@
 from dotenv import load_dotenv
 load_dotenv(override=True)
+
+import nltk
 from nltk.tokenize import sent_tokenize
 from tagging import get_sources_chain
 import streamlit as st
 import pandas as pd
+
+nltk.download('punkt')
 
 st.set_page_config(layout="wide")
 
